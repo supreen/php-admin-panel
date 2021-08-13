963fb6a876dcd75bcaf037e4949145cc53d46557
Suprin Aziz <62968684+supreen@users.noreply.github.com>
2021-08-13 19:39:56 +0500

    Update credential.php
    Now you just need to add edit this file for any change

--- credential.php
+++ credential.php
@@ -3,9 +3,9 @@
 $setadmin="admin"; 
 $setpass="password";
 // you can get the sitekeyfrom https://www.google.com/recaptcha/admin/create . make sure that it is captcha v2 and i am not robot. 
-$sitekey="6LdT6vEUAAAAANxTILnv1lxDZXMR2LqxTPRHaIH_";
+$sitekey="Paste Your Site key here";
 // you can get the secret from https://www.google.com/recaptcha/admin/create . make sure that it is captcha v2 and i am not robot. 
-$secret="6LdT6vEUAAAAAE9RiONnYpYnwtZJeLNi5qgEwfSD";
+$secret="Paste your Secret key here";
 
 
 
